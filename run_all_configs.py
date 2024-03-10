907c7ce14ccca6b29e9985bebd7be19e9e8e6dcc
Tobias Mueller <to.mueller@gmail.com>
2024-03-10 15:49:04 -0700

    initial testing

---
+++ run_all_configs.py
@@ -0,0 +1,18 @@
+import toml
+from pathlib import Path
+from lighttable.imagelooper import ImageLooper
+
+config_files = Path("configs_to_run").rglob("*.toml")
+
+# iterate over each config file
+for cf in config_files:
+    # load the config file
+    c = toml.load(cf)
+
+    # Output directory, create if it doesn't exist
+    Path(c["output"]["path"]).mkdir(parents=True, exist_ok=True)
+
+    # run image processing
+    Looper = ImageLooper(c)
+    Looper.run()
+    print(f"Finished processing {cf.name}")
